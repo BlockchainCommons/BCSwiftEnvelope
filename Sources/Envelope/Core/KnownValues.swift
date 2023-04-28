0015d78efd3c7427ddb9db151a540919152bfcdc
Wolf McNally <wolf@wolfmcnally.com>
2023-04-27 23:08:17 -0700

    Added lookup known value by name.

--- Sources/Envelope/Core/KnownValues.swift
+++ Sources/Envelope/Core/KnownValues.swift
@@ -2,16 +2,18 @@ import Foundation
 
 public struct KnownValues {
     var knownValuesByRawValue: [UInt64: KnownValue]
+    var knownValuesByAssignedName: [String: KnownValue]
     
     public init<T>(_ knownValues: T) where T: Sequence, T.Element == KnownValue {
         knownValuesByRawValue = [:]
+        knownValuesByAssignedName = [:]
         for knownValue in knownValues {
-            Self._insert(knownValue, knownValuesByRawValue: &knownValuesByRawValue)
+            Self._insert(knownValue, knownValuesByRawValue: &knownValuesByRawValue, knownValuesByAssignedName: &knownValuesByAssignedName)
         }
     }
     
     public mutating func insert(_ knownValue: KnownValue) {
-        Self._insert(knownValue, knownValuesByRawValue: &knownValuesByRawValue)
+        Self._insert(knownValue, knownValuesByRawValue: &knownValuesByRawValue, knownValuesByAssignedName: &knownValuesByAssignedName)
     }
     
     public func assignedName(for knownValue: KnownValue) -> String? {
@@ -20,6 +22,10 @@ public struct KnownValues {
     
     public func name(for knownValue: KnownValue) -> String {
         assignedName(for: knownValue) ?? knownValue.name
+    }
+    
+    public func knownValue(named assignedName: String) -> KnownValue? {
+        knownValuesByAssignedName[assignedName]
     }
     
     public static func knownValue(for rawValue: UInt64, knownValues: KnownValues? = nil) -> KnownValue {
@@ -32,12 +38,25 @@ public struct KnownValues {
         return namedKnownValue
     }
     
+    public static func knownValue(named assignedName: String, knownValues: KnownValues? = nil) -> KnownValue? {
+        guard
+            let knownValues,
+            let knownValue = knownValues.knownValuesByAssignedName[assignedName]
+        else {
+            return nil
+        }
+        return knownValue
+    }
+    
     public static func name(for knownValue: KnownValue, knownValues: KnownValues? = nil) -> String {
         knownValues?.name(for: knownValue) ?? knownValue.name
     }
 
-    static func _insert(_ knownValue: KnownValue, knownValuesByRawValue: inout [UInt64: KnownValue]) {
+    static func _insert(_ knownValue: KnownValue, knownValuesByRawValue: inout [UInt64: KnownValue], knownValuesByAssignedName: inout [String: KnownValue]) {
         knownValuesByRawValue[knownValue.value] = knownValue
+        if let name = knownValue.assignedName {
+            knownValuesByAssignedName[name] = knownValue
+        }
     }
 }
 
