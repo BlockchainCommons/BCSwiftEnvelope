3ca5b37c457fc1a20d7de8e979448ecccf0746ff
Wolf McNally <wolf@wolfmcnally.com>
2023-12-22 20:05:41 -0800

    Add default expression operators.

--- Sources/Envelope/Extensions/Expressions/Functions.swift
+++ Sources/Envelope/Extensions/Expressions/Functions.swift
@@ -1,10 +1,21 @@
 import Foundation
 
 public extension Function {
-    static let add = Function(1, "add")
-    static let sub = Function(2, "sub")
-    static let mul = Function(3, "mul")
-    static let div = Function(4, "div")
+    static let add = Function(1, "add") // addition
+    static let sub = Function(2, "sub") // subtraction
+    static let mul = Function(3, "mul") // multiplication
+    static let div = Function(4, "div") // division
+    static let neg = Function(5, "neg") // unary negation
+    static let lt = Function(6, "lt") // less than
+    static let le = Function(7, "le") // less than or equal to
+    static let gt = Function(8, "gt") // greater than
+    static let ge = Function(9, "ge") // greater than or equal to
+    static let eq = Function(10, "eq") // equal to
+    static let ne = Function(11, "ne") // not equal to
+    static let and = Function(12, "and") // logical and
+    static let or = Function(13, "or") // logical or
+    static let xor = Function(14, "xor") // logical exclusive or
+    static let not = Function(15, "not") // logical not
 }
 
 public var globalFunctions: FunctionsStore = [
@@ -12,4 +23,15 @@ public var globalFunctions: FunctionsStore = [
     .sub,
     .mul,
     .div,
+    .neg,
+    .lt,
+    .le,
+    .gt,
+    .ge,
+    .eq,
+    .ne,
+    .and,
+    .or,
+    .xor,
+    .not
 ]
