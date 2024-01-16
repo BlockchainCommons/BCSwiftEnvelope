22f4583df148caf2d748efca1b9ff8d9b61a8248
Wolf McNally <wolf@wolfmcnally.com>
2024-01-16 15:31:16 -0800

    Aligned to dependencies.

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["Envelope"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/BlockchainCommons/BCSwiftSecureComponents.git", from: "5.0.0"),
+        .package(url: "https://github.com/BlockchainCommons/BCSwiftSecureComponents.git", from: "6.0.0"),
         .package(url: "https://github.com/WolfMcNally/WolfBase", from: "6.0.0"),
         .package(url: "https://github.com/WolfMcNally/Graph.git", from: "1.0.0"),
         .package(url: "https://github.com/WolfMcNally/GraphMermaid.git", from: "1.0.0"),
